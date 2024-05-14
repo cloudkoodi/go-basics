18a66bccc040dd08fa120a716b6796eb8e7bd1fe
cloudkoodi <flamenbaum.dev@gmail.com>
2024-05-14 20:42:49 -0300

    Variables in Go

---
+++ variables_basic/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	// int age = 33; //C++ way
+	var age int = 33
+	fmt.Println("Age:", age)
+
+	var name = "Gabriel"
+	fmt.Println("Your name is:", name)
+
+	_ = name
+
+	s := "Learning goLang!"
+	fmt.Println(s)
+}
