04374dc8393ffda833fbe2cc711c78adb08b0a8b
cloudkoodi <flamenbaum.dev@gmail.com>
2024-06-25 20:59:30 -0300

    Section 20: Functions Defer

---
+++ 20-functions/function-defer/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func foo() {
+	fmt.Println("This is Foo()")
+}
+
+func bar() {
+	fmt.Println("This is bar()")
+}
+
+func foobar() {
+	fmt.Println("This is foobar()")
+}
+
+func main() {
+	defer foo()
+	bar()
+
+	fmt.Println("Just a string after defering foo() and calling bar()")
+
+	defer foobar()
+
+	file, err := os.Open("main.go")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+}
